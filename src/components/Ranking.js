538ba6ece474fc54e7f64ccfbbd12fc0ea4ab5ba
Luis Gabriel <luis.andrade@markts.com.br>
2024-09-03 10:16:15 -0300

    fix

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import goldCrown from '../icons/goldCrown.svg';
-import silverCrown from '../icons/silverCrown.svg';
-import bronzeCrown from '../icons/bronzeCrown.svg';
+import React, { useState, useEffect } from "react";
+
+import goldCrown from "../icons/goldCrown.svg";
+import silverCrown from "../icons/silverCrown.svg";
+import bronzeCrown from "../icons/bronzeCrown.svg";
+import axios from "axios";
 
 const Ranking = () => {
   const [ranking, setRanking] = useState([]);
@@ -13,7 +14,9 @@ const Ranking = () => {
   useEffect(() => {
     const fetchRanking = async () => {
       try {
-        const response = await axios.get('https://brawlhalla-championship-backend.onrender.com/users');
+        const response = await axios.get(
+          "https://brawlhalla-championship-backend.onrender.com/users"
+        );
         setRanking(response.data);
         setLoading(false);
       } catch (err) {
@@ -27,7 +30,9 @@ const Ranking = () => {
 
   useEffect(() => {
     if (ranking.length > 0) {
-      const maxVictories = Math.max(...ranking.map(player => player.victorys));
+      const maxVictories = Math.max(
+        ...ranking.map((player) => player.victorys)
+      );
       const heights = ranking.reduce((acc, player) => {
         acc[player.id] = (player.victorys / maxVictories) * 27; // Ajusta a altura da barra com base nas vitórias
         return acc;
@@ -46,7 +51,7 @@ const Ranking = () => {
     <div style={styles.container}>
       <div style={styles.fileira}>
         <div style={styles.sectionLeft}>
-          {leftPlayers.map(player => (
+          {leftPlayers.map((player) => (
             <div key={player.id} style={styles.player}>
               <img
                 src={player.avatar_url}
@@ -56,7 +61,12 @@ const Ranking = () => {
               <div style={styles.details}>
                 <h2 style={styles.name}>{player.name}</h2>
                 <p style={styles.victories}>{player.victorys} wins</p>
-                <div style={{ ...styles.animationBar, height: `${victoryBarHeights[player.id] || 0}rem` }}></div>
+                <div
+                  style={{
+                    ...styles.animationBar,
+                    height: `${victoryBarHeights[player.id] || 0}rem`,
+                  }}
+                ></div>
               </div>
             </div>
           ))}
@@ -66,9 +76,27 @@ const Ranking = () => {
           {topPlayers.map((player, index) => (
             <div key={player.id} style={styles.player}>
               {/* Adiciona as coroas conforme a posição do jogador */}
-              {index === 0 && <img src={goldCrown} alt="Coroa de Ouro" style={styles.crownGold} />}
-              {index === 1 && <img src={silverCrown} alt="Coroa de Prata" style={styles.crownSilver} />}
-              {index === 2 && <img src={bronzeCrown} alt="Coroa de Bronze" style={styles.crownBronze} />}
+              {index === 0 && (
+                <img
+                  src={goldCrown}
+                  alt="Coroa de Ouro"
+                  style={styles.crownGold}
+                />
+              )}
+              {index === 1 && (
+                <img
+                  src={silverCrown}
+                  alt="Coroa de Prata"
+                  style={styles.crownSilver}
+                />
+              )}
+              {index === 2 && (
+                <img
+                  src={bronzeCrown}
+                  alt="Coroa de Bronze"
+                  style={styles.crownBronze}
+                />
+              )}
 
               <img
                 src={player.avatar_url}
@@ -78,14 +106,19 @@ const Ranking = () => {
               <div style={styles.details}>
                 <h2 style={styles.name}>{player.name}</h2>
                 <p style={styles.victories}>{player.victorys} wins</p>
-                <div style={{ ...styles.animationBar, height: `${victoryBarHeights[player.id] || 0}rem` }}></div>
+                <div
+                  style={{
+                    ...styles.animationBar,
+                    height: `${victoryBarHeights[player.id] || 0}rem`,
+                  }}
+                ></div>
               </div>
             </div>
           ))}
         </div>
 
         <div style={styles.sectionRight}>
-          {rightPlayers.map(player => (
+          {rightPlayers.map((player) => (
             <div key={player.id} style={styles.player}>
               <img
                 src={player.avatar_url}
@@ -95,7 +128,12 @@ const Ranking = () => {
               <div style={styles.details}>
                 <h2 style={styles.name}>{player.name}</h2>
                 <p style={styles.victories}>{player.victorys} wins</p>
-                <div style={{ ...styles.animationBar, height: `${victoryBarHeights[player.id] || 0}rem` }}></div>
+                <div
+                  style={{
+                    ...styles.animationBar,
+                    height: `${victoryBarHeights[player.id] || 0}rem`,
+                  }}
+                ></div>
               </div>
             </div>
           ))}
@@ -107,112 +145,112 @@ const Ranking = () => {
 
 const styles = {
   container: {
-    position: 'fixed',
+    position: "fixed",
     bottom: 0,
     left: 0,
-    width: '100%',
-    boxSizing: 'border-box',
+    width: "100%",
+    boxSizing: "border-box",
   },
   fileira: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    width: '100%',
-    maxWidth: '1200px',
-    margin: 'auto',
-    gap: '50px',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-end",
+    width: "100%",
+    maxWidth: "1200px",
+    margin: "auto",
+    gap: "50px",
   },
   sectionLeft: {
-    display: 'flex',
-    flexDirection: 'row-reverse',
-    justifyContent: 'center',
-    alignItems: 'end',
-    gap: '40px',
-    width: '320px',
-    padding: '1px',
-    position: 'relative',
+    display: "flex",
+    flexDirection: "row-reverse",
+    justifyContent: "center",
+    alignItems: "end",
+    gap: "40px",
+    width: "320px",
+    padding: "1px",
+    position: "relative",
   },
   sectionCenter: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'end',
-    gap: '60px',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "end",
+    gap: "60px",
     flex: 1,
-    padding: '1px',
-    borderRadius: '10px',
-    position: 'relative',
+    padding: "1px",
+    borderRadius: "10px",
+    position: "relative",
   },
   sectionRight: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'end',
-    gap: '60px',
-    width: '400px',
-    padding: '1px',
-    borderRadius: '10px',
-    position: 'relative',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "end",
+    gap: "60px",
+    width: "400px",
+    padding: "1px",
+    borderRadius: "10px",
+    position: "relative",
   },
   player: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    position: 'relative',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    position: "relative",
   },
   avatar: {
-    width: '100px',
-    height: '100px',
-    borderRadius: '50%',
-    marginBottom: '5px',
-    border: '3px solid black', 
+    width: "100px",
+    height: "100px",
+    borderRadius: "50%",
+    marginBottom: "5px",
+    border: "3px solid black",
   },
   details: {
-    textAlign: 'center',
-    color: '#000000',
+    textAlign: "center",
+    color: "#000000",
   },
   name: {
-    fontSize: '20px',
-    fontWeight: 'bold',
-    margin: '0',
+    fontSize: "20px",
+    fontWeight: "bold",
+    margin: "0",
   },
   victories: {
-    fontSize: '15px',
-    color: '#000000',
-    fontWeight: 'bold',
-    margin: '0',
-    marginTop: '5px',
+    fontSize: "15px",
+    color: "#000000",
+    fontWeight: "bold",
+    margin: "0",
+    marginTop: "5px",
   },
   animationBar: {
-    width: '70px',
+    width: "70px",
     height: 0,
-    backgroundColor: '#730065',
-    transition: 'height 1s ease-in-out',
-    marginTop: '5px',
+    backgroundColor: "#730065",
+    transition: "height 1s ease-in-out",
+    marginTop: "5px",
   },
   crownGold: {
-    position: 'absolute',
-    top: '-37px',
-    left: '34%',
-    transform: 'translateX(-50%) rotate(-15deg)',
-    width: '60px',
-    height: 'auto',
+    position: "absolute",
+    top: "-37px",
+    left: "34%",
+    transform: "translateX(-50%) rotate(-15deg)",
+    width: "60px",
+    height: "auto",
   },
   crownSilver: {
-    position: 'absolute',
-    top: '-40px',
-    left: '64%',
-    transform: 'translateX(-50%) rotate(-10deg)',
-    width: '60px',
-    height: 'auto',
+    position: "absolute",
+    top: "-40px",
+    left: "64%",
+    transform: "translateX(-50%) rotate(-10deg)",
+    width: "60px",
+    height: "auto",
   },
   crownBronze: {
-    position: 'absolute',
-    top: '-30px',
-    left: '14%',
-    transform: 'translateX(-50%) rotate(-5deg)',
-    width: '60px',
-    height: 'auto',
+    position: "absolute",
+    top: "-30px",
+    left: "14%",
+    transform: "translateX(-50%) rotate(-5deg)",
+    width: "60px",
+    height: "auto",
   },
 };
 
